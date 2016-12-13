bb7b168a80d86421b103b30ff3ebc78e75d1de62
Ruben <ruben_barr@hotmail.com>
2016-12-14 00:26:56 +0100

    Añado el servicio de mostPopularCensus, el cual devuelve una lista con los 10 censos con más participantes.

--- CensoEGC/src/main/java/services/CensusService.java
+++ CensoEGC/src/main/java/services/CensusService.java
@@ -3,9 +3,15 @@ package services;
 import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -566,6 +572,62 @@ public class CensusService {
 				}
 			}
 			return finished;
+		}
+
+		
+		/**
+		 * Método para obtener los 10 censos con mayor participación
+		 * 
+		 */
+		 public Map<Census, Integer> mostPopularCensus(){
+			 Collection<Census>censos=censusRepository.findAll();
+			 Map<Census,Integer>res= new HashMap<Census,Integer>();
+			 Map<Census,Integer>result= new HashMap<Census,Integer>();
+			 for(Census c:censos){
+				 Integer cont=0;
+				 for(String s:c.getVotoPorUsuario().keySet()){
+					 if(c.getVotoPorUsuario().get(s)==true){
+						cont=cont+1;
+					 }
+				 }
+				 res.put(c, cont);
+			 }
+			 res = sortByValue(res);
+			 Iterator<Census> c = res.keySet().iterator();
+			 for(int i=0;i<10;i++){
+				 if(c.hasNext()){
+					 result.put((Census)c, res.get(c));
+				 }
+			 }
+			 return result;
+		 }
+		 
+		 /**
+		  * Método para comparar la participación de los censos para poder ordenarlos
+		  * @param censos
+		  * @return censos ordenados
+		  */
+		 
+		 public static <K, V extends Comparable<? super V>> Map<K, V> 
+		    sortByValue( Map<K, V> map )
+		{
+		    List<Map.Entry<K, V>> list =
+		        new LinkedList<>( map.entrySet() );
+		    Collections.sort( list, new Comparator<Map.Entry<K, V>>()
+		    {
+		        @Override
+		        public int compare( Map.Entry<K, V> o1, Map.Entry<K, V> o2 )
+		        {
+		            return ( o2.getValue() ).compareTo( o1.getValue() );
+		        }
+		    } );
+
+		    Map<K, V> result = new LinkedHashMap<>();
+		    for (Map.Entry<K, V> entry : list)
+		    {
+		        result.put( entry.getKey(), entry.getValue() );
+		    }
+		    return result;
 		}
 
 }
